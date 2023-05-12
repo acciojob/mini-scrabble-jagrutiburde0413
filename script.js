62b89ec9917a4517ddcbd5d69486952ab5cbf8f4
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-05-12 19:16:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //your code here
+// const evaluatedText = document.getElementById("evaluatedText");
+// const letterCount = document.getElementById("letterCount");
+
+// evaluatedText.addEventListener("input", function() {
+//   const text = evaluatedText.value;
+//   const count = text.length;
+//   letterCount.textContent = count;
+// });
 const evaluatedText = document.getElementById("evaluatedText");
 const letterCount = document.getElementById("letterCount");
 
+const countLetters = (text) => {
+  let count = 0;
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== " ") {
+      count++;
+    }
+  }
+  return count;
+};
+
 evaluatedText.addEventListener("input", function() {
   const text = evaluatedText.value;
-  const count = text.length;
+  const count = countLetters(text);
   letterCount.textContent = count;
 });
